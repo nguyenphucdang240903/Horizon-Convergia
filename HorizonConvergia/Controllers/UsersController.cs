631261ddd25f09651e5616b6e96aad44b6dc6fec
NguyenPhuc <dangnguyenphucdnp@gmail.com>
2025-06-29 21:45:38 +0700

    authen

--- HorizonConvergia/Controllers/UsersController.cs
+++ HorizonConvergia/Controllers/UsersController.cs
@@ -34,7 +34,7 @@ namespace HorizonConvergia.Controllers
             });
         }
         [HttpPost("admin-create")]
-        //[Authorize(Policy = "Admin")]
+        [Authorize(Policy = "Admin")]
         public async Task<IActionResult> AdminCreateUser([FromBody] RegisterUserDTO dto)
         {
             var user = await _userService.AdminCreateUserAsync(dto);
